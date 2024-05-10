aa6bb08b0daa75f6df686c786ae00468bb021749
GerasH-Solicy <geras.harutyunyan@solicy.net>
2024-05-10 19:01:12 +0400

    Added some minor thing on serializer

--- shop/base/serializers/user.py
+++ shop/base/serializers/user.py
@@ -8,5 +8,8 @@ def userSerializer(data: User):
     result['email'] = data.email
     result['image'] = data.image
     result['token'] = data.token
+    result['rate'] = data.rate
+    result['birthDate'] = data.birthDate.strftime('%Y-%m-%d')
+    result['role'] = data.role.name if data.role else ""
 
     return result
